e32c682725669f21d5143c5435d7428980305ab6
ajmengistu <a_mengistu@outlook.com>
2019-06-23 17:41:59 -0400

    Added about page

--- src/organ_match_app/matchapp/urls.py
+++ src/organ_match_app/matchapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='matchapp-home'),
+    # path('about/', views.about, name='matchapp-about'),
     path('about/', views.about, name='matchapp-about'),
     path('request/', views.request_organ, name='matchapp-request'),
     path('offer/', views.offer_organ, name='matchapp-offer'),
